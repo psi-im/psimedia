a44543183b63732cd1f82871592df53782e08102
Justin Karneges <justin@affinix.com>
2008-12-09 22:45:47 +0000

    forgot to add these files
    git-svn-id: https://delta.affinix.com/svn/trunk/psimedia@360 ac7a513c-5e3a-0410-af42-968c3ac6e7dc

---
+++ gstprovider/gstelements/gstelements.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2008  Barracuda Networks, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ *
+ */
+
+#include "gstelements.h"
+
+void gstelements_rtpmanager_register();
+
+#ifdef Q_OS_WIN
+void gstelements_directsound_register();
+void gstelements_winks_register();
+#endif
+
+#ifdef Q_OS_MAC
+void gstelements_osxaudio_register();
+void gstelements_osxvideo_register();
+#endif
+
+void gstelements_register()
+{
+	gstelements_rtpmanager_register();
+
+#ifdef Q_OS_WIN
+	gstelements_directsound_register();
+	gstelements_winks_register();
+#endif
+
+#ifdef Q_OS_MAC
+	gstelements_osxaudio_register();
+	gstelements_osxvideo_register();
+#endif
+}
