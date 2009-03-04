a0ad3c1a6581faa7798058a32a1485fe13b7261b
Justin Karneges <justin@affinix.com>
2009-03-04 23:20:43 +0000

    add end-point resampling to 22KHz back in (needed for speexdsp)
    git-svn-id: https://delta.affinix.com/svn/trunk/psimedia@502 ac7a513c-5e3a-0410-af42-968c3ac6e7dc

--- gstprovider/pipeline.cpp
+++ gstprovider/pipeline.cpp
@@ -160,8 +160,8 @@ static GstElement *make_devicebin(const QString &id, PDevice::Type type, const Q
 		GstElement *capsfilter = gst_element_factory_make("capsfilter", NULL);
 		GstCaps *caps = gst_caps_new_empty();
 		GstStructure *cs = gst_structure_new("audio/x-raw-int",
-			/*"rate", G_TYPE_INT, 22050,
-			"width", G_TYPE_INT, 16,*/ NULL);
+			"rate", G_TYPE_INT, 22050,
+			"width", G_TYPE_INT, 16, NULL);
 		gst_caps_append_structure(caps, cs);
 		g_object_set(G_OBJECT(capsfilter), "caps", caps, NULL);
 		gst_caps_unref(caps);
@@ -326,8 +326,8 @@ public:
 			capsfilter = gst_element_factory_make("capsfilter", NULL);
 			GstCaps *caps = gst_caps_new_empty();
 			GstStructure *cs = gst_structure_new("audio/x-raw-int",
-				/*"rate", G_TYPE_INT, 22050,
-				"width", G_TYPE_INT, 16,*/ NULL);
+				"rate", G_TYPE_INT, 22050,
+				"width", G_TYPE_INT, 16, NULL);
 			gst_caps_append_structure(caps, cs);
 			g_object_set(G_OBJECT(capsfilter), "caps", caps, NULL);
 			gst_caps_unref(caps);
