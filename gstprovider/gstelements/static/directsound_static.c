2f10c6e65f460cbd3641a23a4a9df105e1a997dc
Justin Karneges <justin@affinix.com>
2009-06-02 04:04:52 +0000

    update static loader to match dynamic
    git-svn-id: https://delta.affinix.com/svn/trunk/psimedia@615 ac7a513c-5e3a-0410-af42-968c3ac6e7dc

--- gstprovider/gstelements/static/directsound_static.c
+++ gstprovider/gstelements/static/directsound_static.c
@@ -1,28 +1,29 @@
-/*
- * Copyright (C) 2005 Sebastien Moutte <sebastien@moutte.net>
- * Copyright (C) 2007 Pioneers of the Inevitable <songbird@songbirdnest.com>
- * Copyright (C) 2008 Barracuda Networks, Inc.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- *
- * The development of this code was made possible due to the involvement
- * of Pioneers of the Inevitable, the creators of the Songbird Music player
- *
- */
+/* GStreamer
+* Copyright (C) 2005 Sebastien Moutte <sebastien@moutte.net>
+* Copyright (C) 2007 Pioneers of the Inevitable <songbird@songbirdnest.com>
+*
+* gstdirectsoundplugin.c:
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the
+* Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+* Boston, MA 02111-1307, USA.
+* 
+*
+* The development of this code was made possible due to the involvement
+* of Pioneers of the Inevitable, the creators of the Songbird Music player
+*
+*/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -30,6 +31,8 @@
 
 #include "../directsound/gstdirectsoundsink.h"
 #include "../directsound/gstdirectsoundsrc.h"
+
+GST_DEBUG_CATEGORY (directsound_debug);
 
 static gboolean
 plugin_init (GstPlugin * plugin)
@@ -40,6 +43,9 @@ plugin_init (GstPlugin * plugin)
   if (!gst_element_register (plugin, "directsoundsrc", GST_RANK_PRIMARY,
           GST_TYPE_DIRECTSOUND_SRC))
     return FALSE;
+
+  GST_DEBUG_CATEGORY_INIT (directsound_debug, "directsound", 0,
+        "DirectSound Elements");
 
   return TRUE;
 }
