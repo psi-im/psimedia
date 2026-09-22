68ae28b977ff4905c560dc314ee335d55155b243
Sergei Ilinykh <rion4ik@gmail.com>
2026-09-22 17:38:36 +0300

    Keep live video prep warning-free

--- gstprovider/bins.cpp
+++ gstprovider/bins.cpp
@@ -208,6 +208,7 @@ static bool video_codec_get_recv_elements(const QString &name, GstElement **dec,
 
 GstElement *bins_videoprep_create(const QSize &size, int fps, bool is_live)
 {
+    Q_UNUSED(is_live)
     GstElement *bin = gst_bin_new("videoprepbin");
 
     GstElement *videorate  = nullptr;
